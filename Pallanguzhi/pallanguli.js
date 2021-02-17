d2f62943fbdfd3633caa841b2f89dee37cbd9944
Solai <solaiyappan@student.tce.edu>
2021-02-17 18:51:23 +0530

    pallanguzhi colours changed and tamil text added

--- Pallanguzhi/pallanguli.js
+++ Pallanguzhi/pallanguli.js
@@ -1,34 +1,24 @@
 var homei = true;
-var abouti = false;
 var kuli = new Array(14);
 var begin = false;
 var canPress = false;
 var p1Amount = 0;
 var p2Amount = 0;
 var p1turn;
-var currentv;
+var previousv = 0;
+var currentv = 0;
 var interval;
 interval = setInterval(update);
 setAll(0);
 
 function update() {
-    document.querySelector( '.kuli1' ).innerHTML = kuli[0];
-    document.querySelector( '.kuli2' ).innerHTML = kuli[1];
-    document.querySelector( '.kuli3' ).innerHTML = kuli[2];
-    document.querySelector( '.kuli4' ).innerHTML = kuli[3];
-    document.querySelector( '.kuli5' ).innerHTML = kuli[4];
-    document.querySelector( '.kuli6' ).innerHTML = kuli[5];
-    document.querySelector( '.kuli7' ).innerHTML = kuli[6];
-    document.querySelector( '.kuli8' ).innerHTML = kuli[7];
-    document.querySelector( '.kuli9' ).innerHTML = kuli[8];
-    document.querySelector( '.kuli10' ).innerHTML = kuli[9];
-    document.querySelector( '.kuli11' ).innerHTML = kuli[10];
-    document.querySelector( '.kuli12' ).innerHTML = kuli[11];
-    document.querySelector( '.kuli13' ).innerHTML = kuli[12];
-    document.querySelector( '.kuli14' ).innerHTML = kuli[13];
-    document.querySelector( '.kuli15' ).innerHTML = p1Amount;
-    document.querySelector( '.kuli16' ).innerHTML = p2Amount;
-}
+    var i = 0;
+    for(i=0 ; i<14 ; i++) {
+        document.getElementById( "kuli"+(i+1)).innerHTML = kuli[i];
+        }
+        document.getElementById( "kuli15").innerHTML = p1Amount;
+        document.getElementById( "kuli16").innerHTML = p2Amount;
+    }
 
 function setAll(v) {
     for (i = 0; i < 14; i++) {
@@ -44,18 +34,18 @@ function start() {
         begin = true;
         p1turn = true;
         canPress = true;
-        alert("It's player 1's turn. Select any one valid hole.");
     }
     else if(!canPress) nextTurn();
     else playerturn();
 }
 
 function playerturn(){
-    if(p1turn) alert("It's player 1's turn. Select any one valid hole.");
-    else alert("It's player 2's turn. Select any one valid hole.");
+    if(p1turn) alert("இது பிளேயர் 1 இன் முறை. பூஜ்ஜியமற்ற குழியைத் தேர்ந்தெடுக்கவும்.\nIt's player 1's turn. Select any one non zero hole.");
+    else alert("இது பிளேயர் 2 இன் முறை. பூஜ்ஜியமற்ற குழியைத் தேர்ந்தெடுக்கவும்.\nIt's player 2's turn. Select any one non zero hole.");
 }
 
 function excecute(v){
+    previousv = v;
     var amount = kuli[v];
     var v1;
     kuli[v]=0;
@@ -67,26 +57,47 @@ function excecute(v){
     v = v + amount +1;
     v %= 14;
     currentv = v;
+    
+    var i;
+    for(i=0 ; i<14 ; i++){
+        if(i==previousv)
+            document.getElementById("kuli"+(i+1)).classList = "previousv";
+            else if(i>previousv && i<currentv && previousv < currentv)
+                document.getElementById("kuli"+(i+1)).classList = "kuli3";
+                else if(i>previousv && i != currentv && previousv > currentv)
+                    document.getElementById("kuli"+(i+1)).classList = "kuli3";
+                    else if(i<currentv && previousv > currentv)
+                        document.getElementById("kuli"+(i+1)).classList = "kuli3";
+                        else if(i==currentv)
+                                document.getElementById("kuli"+(i+1)).classList = "currentv";
+                            else if(i<7) 
+                                    document.getElementById("kuli"+(i+1)).classList = "kuli1";
+                                else document.getElementById("kuli"+(i+1)).classList = "kuli2";
+    }
 }
 
 function empty(v){
     var v1= v+1;
     v1%=14;
-    if(p1turn) {p1Amount += kuli[v1]; if(kuli[v1]!=0) alert("Player 1 earns "+ kuli[v1] +" points.");
-    else alert("Player 1 earns no points in this turn.");
-    pasu();
-    alert("It's player 2's turn. Select any one valid hole.");}
-    else {p2Amount += kuli[v1]; if(kuli[v1]!=0) alert("Player 2 earns "+ kuli[v1] +" points.");
-    else alert("Player 2 earns no points in this turn.");
-    pasu();
-    alert("It's player 1's turn. Select any one valid hole.");}
+    if(p1turn) {p1Amount += kuli[v1]; if(kuli[v1]!=0) alert("பிளேயர் 1, "+ kuli[v1] +" புள்ளிகளைப் பெறுகிறார்.\nPlayer 1 earns "+ kuli[v1] +" points.");
+    else alert("பிளேயர் 1 இம்முறை எந்த புள்ளிகளையும் பெறவில்லை.\nPlayer 1 earns no points in this turn.");
+    alert("இது பிளேயர் 2 இன் முறை. பூஜ்ஜியமற்ற குழியைத் தேர்ந்தெடுக்கவும்.\nIt's player 2's turn. Select any one non zero hole.");}
+    else {p2Amount += kuli[v1]; if(kuli[v1]!=0) alert("பிளேயர் 2, "+ kuli[v1] +" புள்ளிகளைப் பெறுகிறார்.\nPlayer 2 earns "+ kuli[v1] +" points.");
+    else alert("பிளேயர் 2 இம்முறை எந்த புள்ளிகளையும் பெறவில்லை.\nPlayer 2 earns no points in this turn.");
+    alert("இது பிளேயர் 1 இன் முறை. பூஜ்ஜியமற்ற குழியைத் தேர்ந்தெடுக்கவும்.\nIt's player 1's turn. Select any one non zero hole.");}
     kuli[v1] = 0;
     p1turn = !p1turn;
     isGameOver();
     canPress = true;
+    for(i=0 ; i<14 ; i++){
+        if(i<7) 
+            document.getElementById("kuli"+(i+1)).classList = "kuli1";
+        else document.getElementById("kuli"+(i+1)).classList = "kuli2";
+}
 }
 
 function nextTurn(){
+    pasu();
     if (kuli[currentv] != 0) excecute(currentv);
     else empty(currentv);
 }
@@ -95,8 +106,8 @@ function pasu(){
     var i = 0;
     for(i=0 ; i<14 ; i++) { //Checking for Pasu
         if(kuli[i]==4){
-            if(i<7) {p1Amount += 4; alert("Player 1 earns a Pasu.");}
-            else {p2Amount += 4; alert("Player 2 earns a Pasu.");}
+            if(i<7) {p1Amount += 4; alert("வீரர் 1 ஒரு பசுவைப் பெறுகிறார்.\nPlayer 1 earns a Pasu.");}
+            else {p2Amount += 4; alert("வீரர் 2 ஒரு பசுவைப் பெறுகிறார்.\nPlayer 2 earns a Pasu.");}
             kuli[i]=0;
         }
     }
@@ -107,12 +118,14 @@ function takethis(v) {
         if((p1turn && v<7) || (!p1turn && v>=7 && v<14)){
             if(kuli[v] != 0){
                 canPress = false;
+                previousv = currentv;
                 excecute(v);
                 isGameOver();
             }
-            else alert("Can't select zero. Select any other holes.")
+            else alert("பூஜ்ஜியத்தைத் தேர்ந்தெடுக்க முடியாது. வேறு எதாவது குழியைத் தேர்ந்தெடுக்கவும்.\nCan't select zero. Select any other holes.")
         }
-        else alert("Do a valid move.");
+        else if(p1turn) alert("இது பிளேயர் 1 இன் முறை.\nIt's Player 1's turn.");
+        else alert("இது பிளேயர் 2 இன் முறை.\nIt's Player 2's turn.")
     }
 }
 
@@ -127,33 +140,20 @@ function isGameOver(){
 }
 
 function gameOver(){
-    if(p1Amount>p2Amount) alert("Player 1 wins. Press OK to play again.");
-    else if(p2Amount>p1Amount) alert("Player 2 wins. Press OK to play again.");
-    else alert("It's a Tie. Press OK to play again.");
+    if(p1Amount>p2Amount) alert("பிளேயர் 1 வென்றார். மீண்டும் விளையாட OK வை அழுத்தவும்.\nPlayer 1 wins. Press OK to play again.");
+    else if(p2Amount>p1Amount) alert("பிளேயர் 2 வென்றார். மீண்டும் விளையாட OK வை அழுத்தவும்.\nPlayer 2 wins. Press OK to play again.");
+    else alert("போட்டி சமநிலையில் முடிந்தது. மீண்டும் விளையாட OK என்பதை அழுத்தவும்.\nIt's a Tie. Press OK to play again.");
     begin = false;
 }
 
 function game(){
-    document.getElementById("board").style.display = "inline-block";
-    document.getElementById("about").style.display = "none";
     if(!homei){
-    document.getElementById("homei").classList.toggle("active");
-    homei = !homei;
+        document.getElementById("board").style.display = "inline-block";
+        document.getElementById("about").style.display = "none";
     }
-    if(abouti){
-    document.getElementById("abouti").classList.toggle("active");
-    abouti = !abouti;
+    else {
+        document.getElementById("board").style.display = "none";
+        document.getElementById("about").style.display = "contents";
     }
-}
-function info(){
-    document.getElementById("board").style.display = "none";
-    document.getElementById("about").style.display = "contents";
-    if(homei){
-    document.getElementById("homei").classList.toggle("active");
-    homei = !homei;
-    }
-    if(!abouti){
-    document.getElementById("abouti").classList.toggle("active");
-    abouti = !abouti;
-    }
+    homei=!homei;
 }
